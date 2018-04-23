540d8278a8ac0a880c1bab902673183fdb51ac73
neimasilk <neimasilk@yahoo.com>
2018-04-23 09:03:38 +0800

    menambah word2vec_preprocess

--- kompas.py
+++ kompas.py
@@ -7,14 +7,17 @@ import string
 import re
 from urllib import request
 from bs4 import BeautifulSoup
+# import _pickle as pickle
+from unidecode import unidecode
 
 
 class Kompas:
-    def tokenSaja(self, teks):
+    def token_saja(self, teks):
         teksToken = nltk.word_tokenize(teks)
         teks = nltk.Text(teksToken)
         kecil = [w.lower() for w in teks]
-        nokompas = [w for w in kecil if w != "kompas.com"]
+        no_ascii = [self.remove_non_ascii(huruf) for huruf in kecil]
+        nokompas = [w for w in no_ascii if w != "kompas.com"]
         nopuncnonumber1 = [x for x in nokompas if not re.fullmatch('[' + string.punctuation + ']+', x)]
         nopuncnonumber2 = [x for x in nopuncnonumber1 if not re.search(r'\d+', x)]
         nopuncnonumber3 = [x for x in nopuncnonumber2 if not re.search(r'\=', x)]
@@ -22,7 +25,7 @@ class Kompas:
         isi = "".join([" " + i for i in nopuncnonumber]).strip()
         return isi
 
-    def getContent(self, url):
+    def get_content(self, url):
         html = request.urlopen(url).read().decode('utf-8')
         rawhtml = BeautifulSoup(html, "html5lib")
         for script in rawhtml(["script", "style"]):
@@ -31,10 +34,10 @@ class Kompas:
         headline = rawheadline[0].get_text()
         rawnewsbody = rawhtml.select("[class^=read__content]")
         newsbody = rawnewsbody[0].get_text()
-        isi = self.tokenSaja(newsbody)
+        isi = self.token_saja(newsbody)
         return headline, isi
 
-    def kompasIndex(self,tgl):
+    def kompas_index(self, tgl):
         urltemp = "http://indeks.kompas.com/news/"
         tanggal = tgl
         indexPages = ""
@@ -69,9 +72,47 @@ class Kompas:
                 link.append(link1['href'])
         return link
 
+    def remove_non_ascii(self, text):
+        return unidecode(str(text))
+
+    def save_token(self, tokens, namafile):
+        # mengembalikan text dari token
+        join_seluruh = ""
+        for i in tokens:
+            joinIsi = "".join(" " + j for j in i)
+            join_seluruh = join_seluruh + " " + joinIsi
+        saveFile = open(namafile, 'w')
+        join_seluruh = self.remove_non_ascii(join_seluruh)
+        saveFile.write(join_seluruh)
+        saveFile.close()
+        # f = open(namafile + ".pkl", 'wb')
+        # pickle.dump(tokens, f)
+        # f.close()
+        return join_seluruh
+
+    def kompas_leech(self, daftar, namafile, save=True, tampil=False):
+        tokens = []
+        for i in daftar:
+            if tampil:
+                print(i)
+            try:
+                header, isi = self.get_content(i)
+                token = nltk.word_tokenize(isi)
+                # print(token[1:])
+                # hilangkan kata kompas.com dan kota dimana diberitakan
+                tokens.append(token[1:])
+            except:
+                pass
+        if save:
+            self.save_token(tokens, namafile)
+        return tokens
+
 
 if __name__ == "__main__":
     kompas = Kompas()
-    tanggal = "2014-01-01"
-    daftar = kompas.kompasIndex(tanggal)
-    print(daftar)
+    tanggal = "2015-04-21"
+    daftar = kompas.kompas_index(tanggal)
+    kompas.kompas_leech(daftar, "save.txt", True, True)
+
+
+
---
+++ word2vec_preprocess.py
@@ -0,0 +1,4 @@
+# membuat interface untuk memproses word2vec sebelum dilatih ke word2vec, untuk semua data
+# kompas, wikipedia, twitter
+
+# generate data, permutasi dll.. dari kata
