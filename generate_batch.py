f0abaec1ff52f9f057f1febcd8757340a34fb009
mukhlis <neimasilk@gmail.com>
2018-09-11 11:49:23 +0800

    nambah comment generate batch

--- generate_batch.py
+++ generate_batch.py
@@ -1,3 +1,6 @@
+# generate_batch adalah step ke 3 untuk model skip-gram di word vector untuk mempermudah proses.
+# bisa dijalankan kalau step 1 dan 2 selesais
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -13,41 +16,41 @@ from six.moves import urllib
 from six.moves import xrange  # pylint: disable=redefined-builtin
 import pickle
 
-
-with open('data_count_dictionary_reverse_dictionary.pkl','rb') as f:  # Python 3: open(..., 'rb')
+with open('data_count_dictionary_reverse_dictionary.pkl', 'rb') as f:  # Python 3: open(..., 'rb')
     data, count, dictionary, reverse_dictionary = pickle.load(f)
 
 data_index = 0
 
+
 # Step 3: Function to generate a training batch for the skip-gram model.
 def generate_batch(batch_size, num_skips, skip_window):
-  global data_index
-  assert batch_size % num_skips == 0
-  assert num_skips <= 2 * skip_window
-  batch = np.ndarray(shape=(batch_size), dtype=np.int32)
-  labels = np.ndarray(shape=(batch_size, 1), dtype=np.int32)
-  span = 2 * skip_window + 1  # [ skip_window target skip_window ]
-  buffer = collections.deque(maxlen=span)
-  for _ in range(span):
-    buffer.append(data[data_index])
-    data_index = (data_index + 1) % len(data)
-  for i in range(batch_size // num_skips):
-    target = skip_window  # target label at the center of the buffer
-    targets_to_avoid = [skip_window]
-    for j in range(num_skips):
-      while target in targets_to_avoid:
-        target = random.randint(0, span - 1)
-      targets_to_avoid.append(target)
-      batch[i * num_skips + j] = buffer[skip_window]
-      labels[i * num_skips + j, 0] = buffer[target]
-    buffer.append(data[data_index])
-    data_index = (data_index + 1) % len(data)
-  # Backtrack a little bit to avoid skipping words in the end of a batch
-  data_index = (data_index + len(data) - span) % len(data)
-  return batch, labels
+    global data_index
+    assert batch_size % num_skips == 0
+    assert num_skips <= 2 * skip_window
+    batch = np.ndarray(shape=(batch_size), dtype=np.int32)
+    labels = np.ndarray(shape=(batch_size, 1), dtype=np.int32)
+    span = 2 * skip_window + 1  # [ skip_window target skip_window ]
+    buffer = collections.deque(maxlen=span)
+    for _ in range(span):
+        buffer.append(data[data_index])
+        data_index = (data_index + 1) % len(data)
+    for i in range(batch_size // num_skips):
+        target = skip_window  # target label at the center of the buffer
+        targets_to_avoid = [skip_window]
+        for j in range(num_skips):
+            while target in targets_to_avoid:
+                target = random.randint(0, span - 1)
+            targets_to_avoid.append(target)
+            batch[i * num_skips + j] = buffer[skip_window]
+            labels[i * num_skips + j, 0] = buffer[target]
+        buffer.append(data[data_index])
+        data_index = (data_index + 1) % len(data)
+    # Backtrack a little bit to avoid skipping words in the end of a batch
+    data_index = (data_index + len(data) - span) % len(data)
+    return batch, labels
+
 
 batch, labels = generate_batch(batch_size=8, num_skips=2, skip_window=1)
 
 with open('batch__labels__data_index.pkl', 'wb') as f:  # Python 3: open(..., 'wb')
     pickle.dump([batch, labels, data_index], f)
-
